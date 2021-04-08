3ca3705e55778383513f6278e53069a0c91f5a25
Juan Ignacio Ibarbia <70922215+PiteIbarbia@users.noreply.github.com>
2021-04-08 19:21:00 -0300

    Juan 08/04/21

---
+++ telegramBot/functions/index.js
@@ -0,0 +1,17 @@
+const functions = require("firebase-functions");
+const { Telegraf } = require('telegraf')
+
+const bot = new Telegraf('1626545132:AAHH66LBc-rfSX1YQK9XoUfc6DWQ5Ic0ea8')
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
